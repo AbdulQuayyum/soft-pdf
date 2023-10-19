113fb6b47790392265f6cb3083f08aa99c19632c
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-10-19 16:13:30 +0100

    Getting project ready...

---
+++ src/Utilities/Utilities.ts
@@ -0,0 +1,51 @@
+import { Metadata } from 'next'
+import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx"
+ 
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs))
+}
+
+export function constructMetadata({
+  title = "Soft-PDF || Makes Understanding PDF Easier.",
+  description = "Soft-PDF is an open-source software to makes understanding PDFs files easier.",
+  image = "/logo.png",
+  icons = "/logo.png",
+  noIndex = false
+}: {
+  title?: string
+  description?: string
+  image?: string
+  icons?: string
+  noIndex?: boolean
+} = {}): Metadata {
+  return {
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      images: [
+        {
+          url: image
+        }
+      ]
+    },
+    twitter: {
+      card: "summary_large_image",
+      title,
+      description,
+      images: [image],
+      creator: "@TheAbdulQuayyum"
+    },
+    icons,
+    metadataBase: new URL('http://localhost:3000/'),
+    themeColor: '#FFF',
+    ...(noIndex && {
+      robots: {
+        index: false,
+        follow: false
+      }
+    })
+  }
+}  
