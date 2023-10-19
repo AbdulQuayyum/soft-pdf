bf412c608ef6d38852a4c176838ca17582401519
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-10-19 16:36:28 +0100

    Cooking, but not a Chef👨🏾‍🍳

---
+++ README.md
@@ -0,0 +1,8 @@
+# Soft-PDF - Makes Understanding PDF easier
+
+###### Soft-PDF is an open-source software to make understanding PDFs files easier.
+
+
+## License
+
+[MIT](https://choosealicense.com/licenses/mit/)
---
+++ src/Assets/Index.ts
@@ -0,0 +1,13 @@
+import automatic from './automation.png'
+import easy from './easy.png'
+import productive from './productive.png'
+import search from './search.png'
+import understanding from './understanding.png'
+
+export {
+  automatic,
+  easy,
+  productive,
+  search,
+  understanding,
+}
