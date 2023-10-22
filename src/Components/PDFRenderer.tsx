00885b711f08d33015ee3fe86b76df91e1228c5d
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-10-22 11:58:52 +0100

    Working on the Dashboard (the uploading feature)...

---
+++ src/Components/PDFRenderer.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const PDFRenderer = () => {
+  return (
+    <div className="w-full bg-white rounded-md shadow flex flex-col items-center">
+      <div className="h-14 w-full border-b border-zinc-200 flex items-center justify-between px-2">
+        PDFRenderer
+      </div>
+      <div className="flex-1 w-full max-h-screen">PDFRenderer</div>
+      PDFRenderer
+    </div>
+  )
+}
+
+export default PDFRenderer
