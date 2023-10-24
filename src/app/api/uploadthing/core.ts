49c9df401c187aaeb1bcab4e0d947966dc02068d
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-10-24 18:22:53 +0100

    Working on the Dashboard(the chat system, the logic behind the project)...

--- src/app/api/uploadthing/core.ts
+++ src/app/api/uploadthing/core.ts
@@ -1,20 +1,24 @@
 import { createUploadthing, type FileRouter } from "uploadthing/next";
 import { getKindeServerSession } from '@kinde-oss/kinde-auth-nextjs/server'
+import { PDFLoader } from 'langchain/document_loaders/fs/pdf'
+import { OpenAIEmbeddings } from 'langchain/embeddings/openai'
+import { PineconeStore } from 'langchain/vectorstores/pinecone'
 
 import { Database } from "@/Database";
- 
+import { getPineconeClient } from "@/Utilities/pinecone";
+
 const f = createUploadthing();
- 
+
 const auth = (req: Request) => ({ id: "fakeId" }); // Fake auth function
- 
+
 export const ourFileRouter = {
   PDFUploader: f({ pdf: { maxFileSize: "4MB" } })
     .middleware(async ({ req }) => {
       const { getUser } = getKindeServerSession()
       const user = getUser()
-    
-      if (!user || !user.id) throw new Error('Unauthorized') 
- 
+
+      if (!user || !user.id) throw new Error('Unauthorized')
+
       return { userId: user.id };
     })
     .onUploadComplete(async ({ metadata, file }) => {
@@ -27,7 +31,57 @@ export const ourFileRouter = {
           uploadStatus: 'PROCESSING',
         },
       })
+
+      try {
+        const response = await fetch(
+          `https://uploadthing-prod.s3.us-west-2.amazonaws.com/${file.key}`
+        )
+
+        const blob = await response.blob()
+
+        const loader = new PDFLoader(blob)
+
+        const pageLevelDocs = await loader.load()
+
+        const pagesAmt = pageLevelDocs.length
+
+        // vectorize and index entire document
+        const pinecone = await getPineconeClient()
+        const pineconeIndex = pinecone.Index('soft-pdf')
+
+        const embeddings = new OpenAIEmbeddings({
+          openAIApiKey: process.env.OPENAI_API_KEY,
+        })
+
+        await PineconeStore.fromDocuments(
+          pageLevelDocs,
+          embeddings,
+          {
+            pineconeIndex,
+            namespace: createdFile.id,
+          }
+        )  
+
+        await Database.file.update({
+          data: {
+            uploadStatus: 'SUCCESS',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        })
+
+      } catch (err) {
+        await Database.file.update({
+          data: {
+            uploadStatus: 'FAILED',
+          },
+          where: {
+            id: createdFile.id,
+          },
+        })
+      }
     }),
 } satisfies FileRouter;
- 
+
 export type OurFileRouter = typeof ourFileRouter;
