0ba1f86c6a8cd0d6df7620749c980af38a6b643e
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-10-28 17:49:02 +0100

    Worked on the logic for get the messages...

--- src/Components/UploadButton.tsx
+++ src/Components/UploadButton.tsx
@@ -99,7 +99,7 @@ const UploadDropzone = () => {
                   <span className="font-semibold">Click to upload</span> or drag
                   and drop
                 </p>
-                <p className="text-xs text-zinc-500">PDF (up to 4MB)</p>
+                <p className="text-xs text-zinc-500">PDF (up to 8MB)</p>
               </div>
 
               {acceptedFiles && acceptedFiles[0] ? (
