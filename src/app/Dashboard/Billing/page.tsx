9a3402c34156d955f24801d044b872730454e7ad
AbdulQuayyum <alaoabdulquayyumm@gmail.com>
2023-10-29 20:38:32 +0100

    Working on the billing...

--- src/app/Dashboard/Billing/page.tsx
+++ src/app/Dashboard/Billing/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 
+import { getUserSubscriptionPlan } from '@/Utilities/userPlan'
 import BillingForm from '@/Components/BillingForm'
 
-const Page = () => {
-  return 
-  <BillingForm />
+const Page = async () => {
+  const subscriptionPlan = await getUserSubscriptionPlan()
+  return <BillingForm subscriptionPlan={subscriptionPlan} />
 }
 
 export default Page
